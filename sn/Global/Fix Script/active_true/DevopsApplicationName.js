1ef6cdcd52f7011f3d26a5ee0057ba729f3a51f4
System Administrator <admin@example.com>
2019-09-30 17:28:58 +0530

    Changes by 'System Administrator' on 'SV_Devops_Application' -- Build #1

---
+++ sn/Global/Fix Script/active_true/DevopsApplicationName.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_script_fix
+ * Created On  : 2019-09-30 11:49:45
+ * Created By  : admin
+ * Updated On  : 2019-09-30 11:49:45
+ * Updated By  : admin
+ * URL         : /sys_script_fix.do?sys_id=a6480b041b5480509614ff37dc4bcb37
+ */
+gs.log("Hey");
