c6f5c4bd3dbe3cbd2b1f1ec7e2cb999c24ffa702
System Administrator <admin@example.com>
2019-11-11 12:49:47 +0530

    Changes by 'System Administrator' on 'SV_DevopsDemo' -- Build #1

---
+++ sn/Global/Fix Script/active_true/Test Devops.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_script_fix
+ * Created On  : 2019-11-11 07:18:23
+ * Created By  : admin
+ * Updated On  : 2019-11-11 07:18:23
+ * Updated By  : admin
+ * URL         : /sys_script_fix.do?sys_id=0bfc966d1b8d40109614ff37dc4bcbc2
+ */
+gs.log("Hello");
