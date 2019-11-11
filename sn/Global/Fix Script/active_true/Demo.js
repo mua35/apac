1e41a027527ea40ff91ff6bd4992ef11d4bc8b2d
System Administrator <admin@example.com>
2019-11-11 14:22:15 +0530

    Changes by 'System Administrator' on 'Devops_KT' -- Build #2

---
+++ sn/Global/Fix Script/active_true/Demo.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_script_fix
+ * Created On  : 2019-11-11 08:51:17
+ * Created By  : admin
+ * Updated On  : 2019-11-11 08:51:17
+ * Updated By  : admin
+ * URL         : /sys_script_fix.do?sys_id=b9427a6d1bcd40109614ff37dc4bcb6a
+ */
+gs.log("Hello Devops");
